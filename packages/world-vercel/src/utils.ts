743b7805dc7eb83df0b41e31e17dc96a36c07fdd
Nathan Rajlich <n@n8.io>
2025-11-24 13:38:41 -0800

    Test workflow-server 102

--- packages/world-vercel/src/utils.ts
+++ packages/world-vercel/src/utils.ts
@@ -110,7 +110,7 @@ export const getHttpUrl = (
   config?: APIConfig
 ): { baseUrl: string; usingProxy: boolean } => {
   const projectConfig = config?.projectConfig;
-  const defaultUrl = 'https://vercel-workflow.com/api';
+  const defaultUrl = 'https://workflow-server-uut8q6vju.vercel.sh/api';
   const defaultProxyUrl = 'https://api.vercel.com/v1/workflow';
   const usingProxy = Boolean(
     config?.baseUrl || (projectConfig?.projectId && projectConfig?.teamId)
